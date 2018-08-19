e358e38cce2edb1e47e4e6d10e5022e16febfc13
Anri Zeneli <anri@seccosquared.com>
2018-08-18 22:40:05 -0400

    same story as last time

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, '../public')));
-
+app.listen(process.env.PORT || 3000);
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response){
