b88b7d448acce5ac92e1e391bca38410bc505c3a
Anri Zeneli <anri@seccosquared.com>
2018-11-04 11:52:39 -0500

    add more config options, such as better outputs from tests. instanlled packages like geckodriver, chromdriver

--- conf.js
+++ conf.js
@@ -1,4 +1,32 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['tests/todo-spec.js']
+  specs: ['tests/e2e/pingBage-spec.js'],
+
+  onPrepare: function(){
+  	// Auto-Pilot is non-angular app
+    browser.ignoreSynchronization = true;
+
+    const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    jasmine.getEnv().addReporter(
+        new SpecReporter({
+            spec: {
+                displayStacktrace: true
+            },
+            colors: {
+    			success: 'green',
+    			failure: 'red',
+    			pending: 'yellow'
+  			},	
+      		prefixes: {
+        		success: '✓ ',
+    			failure: '✗ ',
+    			pending: '* '
+    		}
+        })
+    );
+
+  }, 
+  jasmineNodeOpts: {
+   print: function() {}
+  }
 };
