8a08e3deeae6974bf9bf770b7fecd7cf83e17943
Anri Zeneli <anri@seccosquared.com>
2019-02-23 15:15:43 -0500

    updates to spec

--- tests/e2e/pingBage-spec.js
+++ tests/e2e/pingBage-spec.js
@@ -8,8 +8,7 @@ xdescribe('Site online functionality', function() {
     });
 });
 
-describe('contact functionality', function(){
-
+xdescribe('contact functionality', function(){
 	it('Should test that email address is there', () => {
 		browser.get('https://azeneli.github.io/anrizeneli/');
 		const emailAddress = $$('a#emailAddress-e2eTests');
@@ -25,10 +24,24 @@ describe('contact functionality', function(){
 		expect(profileImage.isPresent()).toBeFalsy();
 	});
 
-	
-
 	afterAll(() => {
         browser.driver.close();
     });
 });
 
+
+describe('social media links', function(){
+
+	it('Should Test that LinkedIn social media icon exists', async () => {
+		browser.get('https://azeneli.github.io/anrizeneli/');
+		browser.sleep(3000);
+		//const linkedIn = $('#linkedin-icon-1');
+		//browser.sleep(3000);
+
+		expect(element(by.id('linkedin-icon-1')).isPresent()).toBe(true);
+		// first step - go to the site
+		// second step - click on element
+		// third step - takes you to link after element clic
+	});
+
+});
